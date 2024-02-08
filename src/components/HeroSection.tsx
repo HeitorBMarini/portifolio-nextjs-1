170f765e0b5cf778ae46ebae5fa4ac0f54049503
HeitorBMVanguard <heitorborba@sistemavanguard.com.br>
2024-02-08 18:51:26 -0300

    add novo import link react-scrool

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,8 +3,7 @@ import React from "react"
 import Image from "next/image"
 
 import { HiArrowDown } from "react-icons/hi"
-import Link from "next/link"
-
+import { Link } from "react-scroll"
 
 const HeroSection = () => {
   return (
